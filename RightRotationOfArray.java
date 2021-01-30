62388a72dc01338ac36c4756c9670def2bf0f106
PandeyRajnish <59505807+PandeyRajnish@users.noreply.github.com>
2021-01-30 07:44:25 +0530

    Update RightRotationOfArray.java

--- RightRotationOfArray.java
+++ RightRotationOfArray.java
@@ -20,9 +20,35 @@ public class RightRotationOfArray {
         
         for(int x:A)
             System.out.print(x+",");
+        System.out.println("");        
+    }
+    
+    
+    //Left Rotation
+    /*
+    public static void main(String[] args) {
+        
+        int A[]={2,67,56,67,78,43,9,65 };
+        
+        for(int x:A)
+            System.out.print(x+",");
         System.out.println("");
         
+        int temp=A[0];
+        for(int i=1;i<A.length;i++){
+            A[i-1]=A[i];
+        }
+        A[A.length-1]=temp;
+        
+        for(int x:A)
+            System.out.print(x+",");
+        System.out.println("");
         
     }
+    
+    */
+    
+}
+
     
 }
